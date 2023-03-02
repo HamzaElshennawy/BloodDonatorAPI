ad841fafd217bf995aab4c1c1cfa657e5b0692b0
Hamza Elshennawy <elshennawyhamza@gmail.com>
2023-03-02 16:12:04 +0200

    Signin Request now working

--- BloodDonatorAPI/Controllers/SignInRequest.cs
+++ BloodDonatorAPI/Controllers/SignInRequest.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BloodDonatorAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 using Npgsql;
 
 namespace BloodDonatorAPI.Controllers
@@ -9,7 +11,8 @@ namespace BloodDonatorAPI.Controllers
     [ApiController]
     public class SignInRequest : ControllerBase
     {
-        
+        User MainUser { get; set; }
+
         [HttpPost]
         public IActionResult Post(string username, string password)
         {
@@ -17,36 +20,43 @@ namespace BloodDonatorAPI.Controllers
         }
         public string GetDataFromServer(string user,string pass)
         {
+            MainUser = new();
             string database = "azure_sys";
             string password = "Hamzamohammed159";
             string ConnectionString = $"Server=blooddonatorserver.postgres.database.azure.com;Database={database};Port=5432;User Id=Elshennawy@blooddonatorserver;Password={password};";
             try
             {
-                // request data from azure database using postgre
 
-                //SqlConnection connection = new SqlConnection(ConnectionString);
                 using (var conn = new NpgsqlConnection(ConnectionString))
                 {
                     conn.Open();
-                    using (var command = new NpgsqlCommand($"SELECT \"UserName\",\"UPassword\" FROM \"Users\" WHERE \"UserName\" = '{user}' AND \"UPassword\" = '{pass}'", conn))
+                    using (var command = new NpgsqlCommand($"SELECT * FROM \"Users\" WHERE \"UserName\" = '{user}' AND \"UPassword\" = '{pass}'", conn))
                     {
                         var result = command.ExecuteReader();
-                        string stringToShow = "";
                         while (result.Read())
                         {
-                            stringToShow += "Username: ";
-                            stringToShow += result.GetString(0);
-                            stringToShow += "\nPassword: ";
-                            stringToShow += result.GetString(1);
+                            MainUser.id = result.GetString(0);
+                            MainUser.UserName = result.GetString(1);
+                            MainUser.Password = result.GetString(2);
+                            MainUser.FirstName = result.GetString(3);
+                            MainUser.LastName = result.GetString(4);
+                            MainUser.Address = result.GetString(5);
+                            MainUser.City = result.GetString(6);
+                            MainUser.Region = result.GetString(7);
+                            MainUser.PhoneNumber = (int)result.GetInt64(8);
+                            MainUser.Age = result.GetInt32(9);
+                            MainUser.Birthdate = result.GetString(10);
+                            MainUser.NationalID = (int)result.GetInt64(11);
+                            MainUser.UserType = result.GetInt16(12);
                         }
-                        return stringToShow;
                     }
-
                 }
+                var UserToJson = JsonConvert.SerializeObject(MainUser);
+                return UserToJson;
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                return e.Message;
+                return ex.Message;
             }
         }
     }
---
+++ BloodDonatorAPI/Models/User.cs
@@ -0,0 +1,59 @@
+﻿using Azure.Core;
+using Newtonsoft.Json;
+using System.Drawing;
+
+namespace BloodDonatorAPI.Models
+{
+    public class User
+    {
+        [JsonProperty("id")]
+        public string id { get; set; }
+
+        [JsonProperty("FirstName")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("LastName")]
+        public string LastName { get; set; }
+
+        [JsonProperty("UserName")]
+        public string UserName { get; set; }
+
+        [JsonProperty("Password")]
+        public string Password { get; set; }
+        [JsonProperty("Address")]
+        public string Address { get; set; }
+
+        [JsonProperty("City")]
+        public string City { get; set; }
+
+        [JsonProperty("Region")]
+        public string Region { get; set; }
+
+        [JsonProperty("PhoneNumber")]
+        public long PhoneNumber { get; set; }
+
+        [JsonProperty("Age")]
+        public int Age { get; set; }
+
+        [JsonProperty("Birthdate")]
+        public string Birthdate { get; set; }
+
+        [JsonProperty("request")]
+        public Request request { get; set; }
+
+        [JsonProperty("NationalID_Image")]
+        public Image NationalID_Image { get; set; }
+
+        [JsonProperty("NationalID")]
+        public long NationalID { get; set; }
+
+        [JsonProperty("PersonalPhoto")]
+        public Image PersonalPhoto { get; set; }
+
+        [JsonProperty("UserType")]
+        public int UserType { get; set; }
+
+        [JsonProperty("RequestHistory")]
+        public List<Request> RequestHistory { get; set; }
+    }
+}
